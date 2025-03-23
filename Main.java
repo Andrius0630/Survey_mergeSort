6deb2244a5531a3989d382038f2f545719c768ab
Andrius0630 <kolendarik123@gmail.com>
2025-03-23 16:36:49 +0200

    main creation

---
+++ Main.java
@@ -0,0 +1,15 @@
+/*
+* andrius.kolenda@mif.stud.vu.lt
+* Purpose: 
+* 2025-03-23
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
